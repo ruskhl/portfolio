87d4d95afb61309b0f7f30cd93204d21d2c7947e
Ruslana <ruslana.khlopuk@gmail.com>
2023-03-01 08:32:09 +0100

    update index and script

--- script.js
+++ script.js
@@ -6,4 +6,6 @@ function NewTabResume() {
     window.open(
     "resume.pdf", "_blank");
 }
-
+function thankyou(){
+    alert("Thank you for getting in touch! I will get back in touch with you soon! Have a great day!!")
+}
